01513a09da200c8b62cee24b0b66e4f6e438006f
Unknown <jackson.grant1998@gmail.com>
2018-10-11 15:25:36 +1000

    Using OOP

--- name-sorter2/Program.cs
+++ name-sorter2/Program.cs
@@ -3,66 +3,74 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
-
-/* 
-
-SOLID FRAMEWORK
-Single responsibility principle[6]
-a class should have only a single responsibility(i.e.changes to only one part of the software's specification should be able to affect the specification of the class).
-Open/closed principle[7]
-"software entities … should be open for extension, but closed for modification."
-Liskov substitution principle[8]
-"objects in a program should be replaceable with instances of their subtypes without altering the correctness of that program." See also design by contract.
-Interface segregation principle[9]
-"many client-specific interfaces are better than one general-purpose interface."[4]
-Dependency inversion principle[10]
-one should "depend upon abstractions, [not] concretions."[4]
-*/
-
-
 namespace namesorted
 {
     class MainClass
     {
+        // Declare variables
+        public string filePath;
+        public List<string> names = new List<string>();
+
         public static void Main(string[] args)
         {
-            // specify when program has begun
+            // Specify when program has begun
             Console.WriteLine("start");
 
-            // specify the path of the file to open. This is only temporary, will use arg in final program.
-            string filePath = args[0];
+            // Specify filepath 
+            string filePath = "/Users/jackson/Desktop/names"; // args[0] <- change to this
 
-            // append the txt file to a T list
-            List<string> names = File.ReadAllLines(filePath).ToList();
+            // Apply functions
+            MainClass nameGetter = new MainClass(filePath);
+            nameGetter.readFile();
+            nameGetter.SortNames();
+            nameGetter.WriteToFile();
 
+            // Alert user that program is complete
+            Console.WriteLine("done");
+        }
+
+        // This is our constructor
+        public MainClass(string filePath1)
+        {
+            filePath = filePath1;
+        }
+
+        // Function to read file
+        public void readFile()
+        {
+            names = File.ReadAllLines(filePath).ToList();
+        }
+
+        // Function to sort names
+        public void SortNames()
+        {
             bool sorted = false;
 
-            //// sort from first to last name
             for (int column = 2; column >= 0; column--)
             {
-            
+
                 while (sorted == false)
                 {
                     sorted = true;
 
-                        for (int i = 1; i < names.Count; i++)
-                        {
+                    for (int i = 1; i < names.Count; i++)
+                    {
 
-                            string[] tempArray2 = names[i].Split(' ');
-                            string[] tempArray1 = names[i - 1].Split(' ');
+                        string[] tempArray2 = names[i].Split(' ');
+                        string[] tempArray1 = names[i - 1].Split(' ');
 
-                            // check to see if array one or two is higher ranked
+                        // check to see if array one or two is higher ranked
                         int result = string.Compare(tempArray1[tempArray1.Length - column], tempArray2[tempArray2.Length - column]);
 
-                            if (result == 1)
-                            {
-                                string tempString = names[i];
-                                names[i] = names[i - 1];
-                                names[i - 1] = tempString;
-                                sorted = false;
-                            }
+                        if (result == 1)
+                        {
+                            string tempString = names[i];
+                            names[i] = names[i - 1];
+                            names[i - 1] = tempString;
+                            sorted = false;
                         }
                     }
+                }
             }
 
             // sort by last name
@@ -80,7 +88,7 @@ namespace namesorted
                     string[] tempArray1 = names[i - 1].Split(' ');
 
                     // check to see if array one or two is higher ranked
-                    int result = string.Compare(tempArray1[tempArray1.Length-1], tempArray2[tempArray2.Length-1]);
+                    int result = string.Compare(tempArray1[tempArray1.Length - 1], tempArray2[tempArray2.Length - 1]);
 
                     if (result == 1)
                     {
@@ -91,18 +99,18 @@ namespace namesorted
                     }
                 }
             }
-
             // print sorted sorted names 
             Console.WriteLine("sorted names");
             names.ForEach(Console.WriteLine);
+        }
 
+        // Function to write to file
+        public void WriteToFile()
 
+        {
             // Write to file
-            File.WriteAllLines("sorted-names-list.txt", names);
+            File.WriteAllLines("/Users/jackson/Desktop⁩", names);
 
-            // Alert user that program is complete
-            Console.WriteLine("done");
         }
     }
 }
-
